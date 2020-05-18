ea76f20fadabfa706db3076dbdb46aad26e7da18
Burak Koken <burakkokenn@gmail.com>
2020-05-18 21:22:28 +0200

    Completed HandlerMethodRegistry and server structs and their implementation have not been completed.

--- method.go
+++ method.go
@@ -11,3 +11,24 @@ const (
 	HttpMethodPut   HttpMethod = http.MethodPut
 	HttpMethodPatch HttpMethod = http.MethodPatch
 )
+
+type HandlerMethodRegistry struct {
+	registerMap map[string][]*HandlerMethod
+}
+
+func NewHandlerMethodRegistry() *HandlerMethodRegistry {
+	return &HandlerMethodRegistry{
+		registerMap: make(map[string][]*HandlerMethod),
+	}
+}
+
+func (registry *HandlerMethodRegistry) Register(handlerMethod *HandlerMethod) {
+	registry.RegisterGroup("", handlerMethod)
+}
+
+func (registry *HandlerMethodRegistry) RegisterGroup(groupName string, handlerMethod *HandlerMethod) {
+	if registry.registerMap[groupName] == nil {
+		registry.registerMap[groupName] = make([]*HandlerMethod, 0)
+	}
+	registry.registerMap[groupName] = append(registry.registerMap[""], handlerMethod)
+}
